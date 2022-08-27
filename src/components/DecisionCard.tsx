73e3db02ce5004e7cf1505e14acbf55a114a6519
Teo Fumagalli <teo.fumagalli@aerolab.co>
2022-08-27 15:19:16 -0300

    v0.2.3: keep back button on end, tweaks to mobile behavior

--- src/components/DecisionCard.tsx
+++ src/components/DecisionCard.tsx
@@ -14,17 +14,64 @@ export interface DecisionCardProps {
   reset: (resetFont: boolean) => void;
 }
 
-const DecisionCard = (props: DecisionCardProps) => {
+export interface DecisionCardHeaderProps {
+  node: DecisionNode | FinalNode;
+  next: string | null;
+  set: (id: string) => void;
+  back: () => void;
+  reset: (resetFont: boolean) => void;
+}
+
+const DecisionCardHeader = ({
+  node,
+  next,
+  set,
+  back,
+  reset,
+}: DecisionCardHeaderProps) => (
+  <div className='dcard-top'>
+    <button className='small' onClick={() => reset(true)}>
+      Inicio
+    </button>
+    {node.id !== '01_serif_sans' &&
+      <button className='small' onClick={() => back()}>
+        Atrás
+      </button>
+    }
+    <div className='dcard-top-space' />
+    {next !== null &&
+      <button
+        className='small dcard-btn-next'
+        onClick={() => set(next)}
+      >
+        Siguiente
+      </button>
+    }
+  </div>
+);
+
+const DecisionCard = ({
+  node,
+  fontName,
+  set,
+  back,
+  reset,
+}: DecisionCardProps) => {
   const [target, setTarget] = useState(0);
   const [next, setNext] = useState<string | null>(null);
 
   useEffect(() => {
     setNext(null);
-  }, [props.node]);
+  }, [node]);
 
   const supportsHover = window.matchMedia('(hover: hover)').matches;
 
-  const select = (e: Event, next: string, targetFrame: number, mobile: boolean) => {
+  const select = (
+    e: Event,
+    next: string,
+    targetFrame: number,
+    mobile: boolean,
+  ) => {
     const btn = e.target as HTMLElement;
     const siblings = btn.parentElement?.children;
     if (siblings) {
@@ -36,10 +83,10 @@ const DecisionCard = (props: DecisionCardProps) => {
     if (mobile) {
       setTarget(targetFrame);
       setNext(next);
-    } else setTimeout(() => props.set(next), 100);
+    } else setTimeout(() => set(next), 100);
   };
 
-  const decisionButtons = (props.node as DecisionNode).options
+  const decisionButtons = (node as DecisionNode).options
     ?.map((option, i, arr) => {
       // Set a default target frame unless a target frame for the option is specified
       let targetFrame: number;
@@ -55,7 +102,14 @@ const DecisionCard = (props: DecisionCardProps) => {
         <button
           key={i}
           onMouseOver={() => setTarget(targetFrame)}
-          onClick={(e) => select(e.nativeEvent, option.leadsTo, targetFrame, !supportsHover)}
+          onClick={(e) =>
+            select(
+              e.nativeEvent,
+              option.leadsTo,
+              targetFrame,
+              !supportsHover && isAnimation(node as DecisionNode),
+            )
+          }
         >
           {option.displayText}
         </button>
@@ -64,29 +118,17 @@ const DecisionCard = (props: DecisionCardProps) => {
 
   return (
     <TransitionGroup className='dcard-transition-group'>
-      {[props.node].map(node => // hack to trigger the transition
+      {[node].map(node => // hack to trigger the transition
         isDecision(node) ? (
           <CSSTransition key={node.id} classNames='card' timeout={500}>
             <div className='dcard-root'>
-              <div className='dcard-top'>
-                <button className='small' onClick={() => props.reset(true)}>
-                  Inicio
-                </button>
-                {props.node.id !== '01_serif_sans' &&
-                  <button className='small' onClick={() => props.back()}>
-                    Atrás
-                  </button>
-                }
-                <div className='dcard-top-space' />
-                {next !== null &&
-                  <button
-                    className='small dcard-btn-next'
-                    onClick={() => props.set(next)}
-                  >
-                    Siguiente
-                  </button>
-                }
-              </div>
+              <DecisionCardHeader
+                node={node}
+                next={next}
+                set={set}
+                back={back}
+                reset={reset}
+              />
 
               <div className='dcard-content'>
                 {isAnimation(node) &&
@@ -113,32 +155,46 @@ const DecisionCard = (props: DecisionCardProps) => {
           </CSSTransition>
         ) : (
           <CSSTransition key={node.id} classNames='card' timeout={500}>
-            <div className='dcard-root dcard-end'>
-              {node.class &&
-                <div>
-                  Clasificaste {props.fontName || 'esta familia'} como:
-                </div>}
-              {node.class &&
-                <div className='dcard-end-class'>
-                  {node.class.split('/').map(part =>
-                    part.charAt(0) === '#' ? (
-                      <em>{part.substring(1)}</em>
-                    ) : (
-                      <span>{part}</span>
-                    )
-                  )}
-                </div>}
+            <div className='dcard-root'>
+              <DecisionCardHeader
+                node={node}
+                next={next}
+                set={set}
+                back={back}
+                reset={reset}
+              />
 
-              {!node.class &&
-                <div>
-                  No pudimos clasificar {props.fontName}. Es posible que no entre en ninguna de las categorías.
-                </div>}
+              <div className='dcard-end'>
+                {node.class
+                  ? (
+                    <>
+                      <div>
+                        Clasificaste {fontName || 'esta familia'} como:
+                      </div>
+                      <div className='dcard-end-class'>
+                        {node.class.split('/').map(part =>
+                          part.charAt(0) === '#' ? (
+                            <em>{part.substring(1)}</em>
+                          ) : (
+                            <span>{part}</span>
+                          )
+                        )}
+                      </div>
+                    </>
+                  )
+                  : (
+                    <div>
+                      No pudimos clasificar {fontName}. Es posible que no entre en ninguna de las categorías.
+                    </div>
+                  )
+                }
 
-              <p>
-                Si pensás que te equivocaste en tu clasificación, podés{' '}
-                <a onClick={() => props.reset(false)}>volver a empezar</a>
-                , o podés <a onClick={() => props.reset(true)}>clasificar otra familia</a>.
-              </p>
+                <p>
+                  Si pensás que te equivocaste en tu clasificación, podés{' '}
+                  <a onClick={() => reset(false)}>volver a empezar</a>
+                  , o podés <a onClick={() => reset(true)}>clasificar otra familia</a>.
+                </p>
+              </div>
             </div>
           </CSSTransition>
         )
